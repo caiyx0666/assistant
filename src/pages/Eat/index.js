d99e185abdcb1f34da3569531c2beb00d0900e96
caiyx0666@gmail.com <caiyx0666@gmail.com>
2021-06-10 17:55:02 +0800

    完成今天吃什么，轮盘抽奖功能

--- src/pages/Eat/index.js
+++ src/pages/Eat/index.js
@@ -1,24 +1,218 @@
 import { Component } from "react";
-import { Button, InputItem } from 'antd-mobile'
+import { InputItem } from 'antd-mobile'
 import './index.scss'
 
 export default class Eat extends Component {
     state = {
+        foodList: [],
+        timer: null,
+        active: 0,
+        playActiveList: [0, 1, 2, 5, 8, 7, 6, 3],
+        timerInt: null
+    }
 
+    componentDidMount = () => {
+        this.getFoods()
+    }
+
+    componentWillUnmount() {
+        this.handleTouchEnd()
+        clearInterval(this.state.timerInt)
+        this.setState({
+            timerInt: null
+        })
+    }
+
+    getFoods = () => {
+        let foods = JSON.parse(window.localStorage.getItem('FOODLIST'));
+        this.setState({
+            foodList: foods ? foods : [{ edit: false }, { edit: false }, { edit: false }, { edit: false }, { edit: false }, { edit: false }, { edit: false }, { edit: false }, { edit: false }]
+        })
+    }
+
+    setFoods = () => {
+        window.localStorage.setItem('FOODLIST', JSON.stringify(this.state.foodList));
+    }
+
+    handleTouchStart(index) {
+        let timer = setTimeout(() => {
+            this.setState({
+                foodList: this.state.foodList.map((e, eIndex) => {
+                    if (index === eIndex) {
+                        e.edit = true;
+                    }
+                    return e;
+                })
+            })
+        }, 1000)
+
+        this.setState({
+            timer: timer
+        })
+
+    }
+
+    handleTouchEnd = () => {
+        clearTimeout(this.state.timer)
+        this.setState({
+            timer: null
+        })
+    }
+
+    handleConfirm(index) {
+        this.setState({
+            foodList: this.state.foodList.map((e, eIndex) => {
+                if (index === eIndex) {
+                    e.edit = false
+                }
+                return e;
+            })
+        }, () => {
+            this.setFoods()
+        })
+    }
+
+    handleChange(index, val) {
+        this.setState({
+            foodList: this.state.foodList.map((e, eIndex) => {
+                if (index === eIndex) {
+                    e.name = val
+                }
+                return e;
+            })
+        })
+    }
+
+    handleBlur(index) {
+        this.setState({
+            foodList: this.state.foodList.map((e, eIndex) => {
+                if (index === eIndex) {
+                    e.edit = false
+                }
+                return e;
+            })
+        }, () => {
+            this.setFoods()
+        })
+    }
+
+    play = () => {
+        if (this.state.timerInt) return;
+        let num = parseInt(6 + Math.random() * 8) * 1000
+        let startTime = new Date().getTime();
+
+        let speed = 200
+
+        let timer = setTimeout(startscroll.bind(this), speed)
+
+        this.setState({
+            timerInt: timer
+        })
+
+        function startscroll() {
+            var nowTime = new Date().getTime();
+            // speed = ((1 - (nowTime - startTime) / num)) * (200 - 20)
+
+            if ((nowTime - startTime) >= num) {
+                clearInterval(timer)
+                timer = null;
+                this.setState({
+                    timerInt: timer
+                })
+                return;
+            }
+
+            if ((nowTime - startTime) <= (num / 4) && speed >= 30) {
+                speed -= 20
+            } else if ((nowTime - startTime) >= (num / 3)) {
+                speed += 50
+            }
+
+            console.log(speed)
+
+            if (this.state.active < 7) {
+                this.setState({
+                    active: this.state.active + 1
+                })
+            } else {
+                this.setState({
+                    active: 0
+                })
+            }
+
+            timer = setTimeout(startscroll.bind(this), speed)
+
+            this.setState({
+                timerInt: timer
+            })
+
+            // 20 200    200-20 = 180  90
+            // 当前速度
+
+        }
+
+        // timer = setInterval(() => {
+        //     speed -= 20;
+
+        //     if (nowTime - startTime >= num) {
+        //         clearInterval(timer)
+        //         this.setState({
+        //             timerInt: null
+        //         })
+        //         return
+        //     }
+
+        //     if (this.state.active < 7) {
+        //         this.setState({
+        //             active: this.state.active + 1
+        //         })
+        //     } else {
+        //         this.setState({
+        //             active: 0
+        //         })
+        //     }
+
+        // }, speed)
+
+        this.setState({
+            timerInt: timer
+        })
     }
 
     render() {
         return (<div className="eat-wrapper">
             <h2>今天吃什么</h2>
             <span style={{ color: '#999', fontSize: '.34375rem' }}>从此不再纠结</span>
-            <div className="add-box">
-                <InputItem
-                    clear
-                    placeholder="还想吃这个？"
-                    ref={el => this.autoFocusInst = el}
-                    className="add-input"
-                >{''}</InputItem>
-                <Button type="primary" inline size="small" className="add-btn">添加</Button>
+            <div className="lucky-draw">
+                {this.state.foodList.map((item, index) => {
+                    if (index === 4) {
+                        return (<div className="eat-select-item" key={index}>
+                            <i className="iconfont icon-kaishi play-btn" onClick={this.play}></i>
+                        </div>)
+                    }
+
+                    if (item.edit) {
+                        return (<div className="eat-select-item" key={index}>
+                            <InputItem
+                                placeholder="输入你想吃的"
+                                value={item.name}
+                                ref={el => this.autoFocusInst = el}
+                                className="eat-input"
+                                onVirtualKeyboardConfirm={this.handleConfirm.bind(this, index)}
+                                onChange={this.handleChange.bind(this, index)}
+                                onBlur={this.handleBlur.bind(this, index)}
+                                clear
+                            >{''}</InputItem>
+                        </div>)
+                    }
+
+                    return (
+                        <div className={`eat-select-item ${this.state.playActiveList[this.state.active] === index ? 'active-item' : ''}`} key={index} onTouchStart={this.handleTouchStart.bind(this, index)} onTouchEnd={this.handleTouchEnd}>
+                            <span>{item.name}</span>
+                        </div>
+                    )
+                })
+                }
             </div>
         </div>)
     }
