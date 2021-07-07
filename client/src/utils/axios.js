eca18e213f061c0dd9bafd1f248e40466d02411f
caiyx0666@gmail.com <caiyx0666@gmail.com>
2021-07-07 16:02:45 +0800

    默认请求头添加token更改为请求拦截器添加token

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -5,16 +5,18 @@ import BASE_URL from './url'
 const $axios = axios.create({
     baseURL: BASE_URL,
     timeout: 5000, // 表示请求的毫秒数，超过该时间，将停止发送请求
-    headers: {
-        Authorization: window.localStorage.getItem('token') ? window.localStorage.getItem('token') : ''
-    }
 })
 
 $axios.interceptors.response.use((config) => {
     if (config.data.status === 401) {
-        Toast.offline('您没有权限登录',2)
+        Toast.offline('请先进行登录',2)
         window.location.href = '/#/login'
     }
+    return config
+})
+
+$axios.interceptors.request.use((config) => {
+    config.headers.Authorization = window.localStorage.getItem('token') ? window.localStorage.getItem('token') : ''
     return config
 })
 
