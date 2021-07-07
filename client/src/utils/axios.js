0ec0050f324d00b8d6c1074fe861fbc4014393f5
caiyx0666@gmail.com <caiyx0666@gmail.com>
2021-07-07 11:45:53 +0800

    添加请求响应，实现权限控制

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
+import { Toast } from 'antd-mobile'
 
 const $axios = axios.create({
     baseURL: 'http://localhost:3001',
     timeout: 5000, // 表示请求的毫秒数，超过该时间，将停止发送请求
+    headers: {
+        Authorization: 'Bearer '+window.localStorage.getItem('token') ? window.localStorage.getItem('token') : ''
+    }
+})
+
+$axios.interceptors.response.use((config) => {
+    if (config.data.status === 401) {
+        Toast.offline('您没有权限登录',2)
+        window.location.href = '/#/login'
+    }
+    return config
 })
 
 export default $axios
