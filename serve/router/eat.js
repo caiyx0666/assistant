c0ded6238ff977099c7b2a3aa818541076c1409d
caiyx0666@gmail.com <caiyx0666@gmail.com>
2021-07-07 11:46:45 +0800

    删除无用代码

--- serve/router/eat.js
+++ serve/router/eat.js
@@ -21,7 +21,6 @@ eatRouter.get('/getEatList', (request, response) => {
 // 更新食物数据
 eatRouter.post('/updateEat', (request, response) => {
     const { body } = request;
-    console.log(body)
     updataEat(body, (err, res) => {
         if (!err) {
             response.json({ status: 200, desc: '操作成功', data: res })
@@ -31,53 +30,6 @@ eatRouter.post('/updateEat', (request, response) => {
     })
 })
 
-// 根据id删除单个食物
-// eatRouter.get('/deleEatById', (request, response) => {
-//     // 获取传过来的参数id
-//     const { id } = request.query;
-//     console.log(id);
-//     // 删除单个食物数据
-//     deleatById(id, (err, results) => {
-//         console.log(results);
-//         if (err === null && results.changedRows != 0) {
-//             response.json({ code: 200, desc: '删除食物数据成功' })
-//         } else {
-//             response.json({ code: 400, desc: '删除食物数据失败' })
-//         }
-//     })
-// })
-
-// 添加食物数据
-// eatRouter.post('/addeat', (request, response) => {
-//     // 获取传过来的参数id
-//     const { body } = request;
-//     console.log(body);
-//     // 删除单个食物数据
-//     addeat(body, (err, results) => {
-//         console.log(results);
-//         if (err === null && results.affectedRows != 0) {
-//             response.json({ code: 200, desc: '添加食物数据成功' })
-//         } else {
-//             response.json({ code: 400, desc: '添加食物数据失败,请传入正确的参数' })
-//         }
-//     })
-// })
-
-// 根据id查找单个食物信息
-// eatRouter.get('/geteatById', (request, response) => {
-//     // 获取传过来的参数id
-//     const { id } = request.query;
-//     console.log(id);
-//     // 删除单个食物数据
-//     geteatById(id, (err, results) => {
-//         console.log(results);
-//         if (err === null) {
-//             response.json({ code: 200, desc: '获取食物数据成功', data: results[0] })
-//         } else {
-//             response.json({ code: 400, desc: '获取食物数据失败' })
-//         }
-//     })
-// })
 
 module.exports = {
     eatRouter
